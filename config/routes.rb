c7a6903ecb0d860d75a28c20ad9b9c0f3b3c3aa3
ryujihagiwara <dragon7103next@icloud.com>
2020-12-02 17:19:33 +0900

    namespace設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  devise_for :admin, controllers: {
+  	sessions:      'admin/sessions',
+  	passwords:     'admin/passwords',
+  	registrations: 'admin/registrations'
+  }
+
+  devise_for :customers, controllers:{
+  	sessions:      'customers/sessions',
+  	passwords:     'customers/passwords',
+  	registrations: 'customers/registrations'
+  }
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
